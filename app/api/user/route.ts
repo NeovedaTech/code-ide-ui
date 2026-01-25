84c3f3402bbd5f9c5a0137620c8e0cf1cf8997b7
Aryan <aryan@rival.io>
2026-01-25 17:40:12 +0530

    Api removed

--- app/api/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/mongodb";
-import User from "@/models/User";
-
-export async function GET() {
-  await connectDB();
-  const users = await User.find();
-  return NextResponse.json(users);
-}
